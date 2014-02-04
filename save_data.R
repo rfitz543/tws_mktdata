f766e3f933248534ed730683da764cb978844eb1
rfitz543 <rfitz543@gmail.com>
2014-02-04 17:30:41 -0500

    add mkt data for CL

--- save_data.R
+++ save_data.R
@@ -1,7 +1,10 @@
-
-ticker <- 'CLH4'
+save_daily <- function(ticker)
+{
 dir <- substr(ticker, 1, 2)
 data <- reqHistoricalData(tws, getContract(ticker), useRTH='0', barSize='1 day',
                           duration='1 Y')
-assign(ticker, data)
+assign(ticker, data, envir=.GlobalEnv)
 saveSymbols.common(ticker, base_dir=dir)
+}
+
+save_daily('CLJ4')
