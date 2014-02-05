e780dc385fee887ada7ef2c206d10aaf786c5143
rfitz543 <rfitz543@gmail.com>
2014-02-04 22:41:07 -0500

    package commit

--- R/save_data.R
+++ R/save_data.R
@@ -1,10 +1,10 @@
 save_daily <- function(ticker)
 {
-dir <- paste('R/', substr(ticker, 1, 2), sep='')
+dir <- substr(ticker, 1, 2)
 data <- reqHistoricalData(tws, getContract(ticker), useRTH='0', barSize='1 day',
                           duration='1 Y')
 assign(ticker, data, envir=.GlobalEnv)
 saveSymbols.days(ticker, base_dir=dir)
 }
 
-save_daily('CLZ4')
+#save_daily('CLZ4')
