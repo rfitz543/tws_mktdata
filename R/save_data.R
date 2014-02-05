a93eff2591d6dd09d417112b4b64e9713be03589
rfitz543 <rfitz543@gmail.com>
2014-02-04 22:52:52 -0500

    package commit

--- R/save_data.R
+++ R/save_data.R
@@ -1,5 +1,6 @@
 save_daily <- function(ticker)
 {
+if(!isConnected(tws)) tws <- twsConnect()
 dir <- substr(ticker, 1, 2)
 data <- reqHistoricalData(tws, getContract(ticker), useRTH='0', barSize='1 day',
                           duration='1 Y')
