3d568cddcc79d6077b652946bf83906e11d08808
rfitz543 <rfitz543@gmail.com>
2014-02-04 22:55:03 -0500

    save function

--- R/save_data.R
+++ R/save_data.R
@@ -1,5 +1,6 @@
 save_daily <- function(ticker)
 {
+require(IBrokers)
 if(!isConnected(tws)) tws <- twsConnect()
 dir <- substr(ticker, 1, 2)
 data <- reqHistoricalData(tws, getContract(ticker), useRTH='0', barSize='1 day',
